500305b562ee65f11e453e43a4f5ee1364a4c2b7
Nyame-Wolf <mumenyam@gmail.com>
2022-09-10 10:12:24 +0300

    add animals to animals table

--- data.sql
+++ data.sql
@@ -1,5 +1,12 @@
 /* Populate database with sample data. */
+INSERT INTO animals (id,name, date_of_birth , weight_kg, neutered, escape_attempts )
+VALUES (1,'Agumon', '2020-02-03',10.23,TRUE,0):
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (id,name, date_of_birth , weight_kg, neutered, escape_attempts )
+VALUES (2,'Gabumon', '2018-11-15',8,TRUE, 2);
+
+INSERT INTO animals (id,name, date_of_birth , weight_kg, neutered, escape_attempts )
+VALUES (3,'Pikachu', '2021-01-07',15.04,FALSE,1);
+
+INSERT INTO animals (id,name, date_of_birth , weight_kg, neutered, escape_attempts )
+VALUES (4,'Devimon', '2017-05-12',11,TRUE,5);
