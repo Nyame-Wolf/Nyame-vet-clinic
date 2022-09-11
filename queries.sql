c5c135e334952f5afd46bbe25bc0e01d8db41ead
Nyame-Wolf <mumenyam@gmail.com>
2022-09-11 08:35:27 +0300

    use tranactions to update species column to unspecified and roll back

--- queries.sql
+++ queries.sql
@@ -1,5 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
+/*Vet clinic database: create animals table*/
+
 SELECT * FROM animals WHERE name LIKE '%mon';
 
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND  '2019-01-01';
@@ -15,3 +17,15 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg  >= 10.4 AND weight_kg <= 17.3;
+
+/* Vet clinic database: query and update animals table */
+/*Task1*/
+-- start a transaction
+BEGIN;
+
+-- update species column to unspecified for all animals
+UPDATE animals
+SET species = 'unspecified';
+
+ROLLBACK;
+
