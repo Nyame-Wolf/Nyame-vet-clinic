2f3f382acf3140046f4b4b0db0845cbfcdea455d
Nyame-Wolf <mumenyam@gmail.com>
2022-09-15 00:03:36 +0300

    create a visits table for visits,vets   and animals

--- schema.sql
+++ schema.sql
@@ -68,4 +68,8 @@ CREATE TABLE specializations (
       PRIMARY KEY( species_id,vet_id) UNIQUE
 );
 
-
+CREATE TABLE visits (
+      animal_id INT NOT NULL   REFERENCES animals(id),
+      vet_id INT NOT NULL  REFERENCES vets(id),
+      visit_date DATE NOT NULL   
+);
