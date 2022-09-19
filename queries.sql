02b2a2d7f233c8a39cc64b0e9a87f46557e3ea5a
Mumenya Nyamu <48546992+Nyame-Wolf@users.noreply.github.com>
2022-09-19 20:43:00 +0300

    fix query between 2016 and 2019

--- queries.sql
+++ queries.sql
@@ -4,7 +4,7 @@
 
 SELECT * FROM animals WHERE name LIKE '%mon';
 
-SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND  '2019-01-01';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND  '2019-12-31';
 
 SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
 
